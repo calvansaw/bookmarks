19bd4715792567b71d64d8e739c9ee5d6e3ed54b
calvansaw <calvansaw@hotmail.com>
2021-03-27 14:00:40 +0800

    test

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 app.use(express.urlencoded({ extended: false })); // extended: false - does not allow nested objects in query strings
 app.use(express.json()); // returns middleware that only parses JSON
 app.use(express.static('public'));
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
 
 /////////////// Controllers /////////////
 app.get('/', (req, res) => {
